51e51190cb0d84177bc03afbd7c228904086ec81
cristian salgado <crito@Seguridad.flowers.local>
2026-05-07 16:15:49 -0500

    Iinitial project setup

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4 shadow-md">
+      <h1 className="text-2xl font-bold">
+        Mesa de Servicios
+      </h1>
+    </header>
+  );
+}
